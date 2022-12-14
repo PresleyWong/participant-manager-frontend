eff2679fe6b994ee1723fbe7615d9d9ca11e8a98
PresleyWong <presley_wg@yahoo.com>
2022-12-14 16:21:01 +0800

    change baseUrl

--- src/redux/api/indexApi.js
+++ src/redux/api/indexApi.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const indexApi = createApi({
   reducerPath: "indexApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/",
-    // baseUrl: "https://participant-manager.fly.dev/api/v1/",
+    // baseUrl: "http://localhost:5000/api/v1/",
+    baseUrl: "https://participant-manager.fly.dev/api/v1/",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
